98204e4863e4132bb7941f1c0cbad7f8f9bd1cf7
dalramjwi <dalramjwi@gmail.com>
2024-07-08 11:01:05 +0900

    output edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 module.exports = {
   entry: "./src/app.js",
-  output: "",
+  output: {
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
