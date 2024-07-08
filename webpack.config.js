2e9d7b05f0f47810ab387e6db05d90192dd0c3f0
dalramjwi <dalramjwi@gmail.com>
2024-07-08 12:56:07 +0900

    css make

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.js|jsx|ts|tsx$/,
         use: "babel-loader",
         exclude: /node_modules/,
@@ -22,6 +26,6 @@ module.exports = {
     //test, use, exclude 세가지 키만 받는다.
     ///\.ts$/ 정규식은 ts인지 확인하는 것
   },
-  resolve: { extensions: [".ts", "js", ".tsx", ".jsx"] },
+  resolve: { extensions: [".ts", "js", ".tsx", ".jsx", "css"] },
   //ts랑 js 쓸거야
 };
